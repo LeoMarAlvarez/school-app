6b64e9990cd670bbaf9979c2a452ca04e82a5896
LEO <alvarezleom@gmail.com>
2021-04-20 12:12:33 -0300

    Installation package fontAwesome

--- src/main.js
+++ src/main.js
@@ -6,14 +6,22 @@ import store from "./store";
 import axios from 'axios';
 import VueAxios from 'vue-axios';
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 // Import Bootstrap an BootstrapVue CSS files (order is important)
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
 
 // Make BootstrapVue available throughout your project
-Vue.use(BootstrapVue)
+Vue.use(BootstrapVue);
 // Optionally install the BootstrapVue icon components plugin
-Vue.use(IconsPlugin)
+Vue.use(IconsPlugin);
+
+library.add(faUserSecret)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.config.productionTip = false;
 Vue.use(VueAxios, axios);
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,10 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
-    <span class="badge badge-primary">Hola Mundo</span>
+    <span class="badge badge-primary">
+      Hola Mundo
+      <font-awesome-icon icon="user-secret" />
+    </span>
   </div>
 </template>
 
